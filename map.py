9f56d7f20bc88227c05bc2c5f783b8233e7a3c61
BenjaminBeggs <45314356+BenjaminBeggs@users.noreply.github.com>
2018-11-24 13:47:51 -0500

    asks for user input

--- map.py
+++ map.py
@@ -3,13 +3,13 @@
 import requests, json
 
 api_key = "AIzaSyClKuWY2YUpxRL_ypzI6CcN-U6KDjtHinY"
-  
+
 # url variable store url 
 urlgeo 	  = "https://maps.googleapis.com/maps/api/geocode/json?"
 urlplaces = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"
 
 # user input of place string, this will be changed to the user input on the website
-address_string = "stittsville ottawa"
+address_string = input("Input the location you're shopping for pizza in (Exact locations and general areas are both acceptable).")
 
 # requests relevant latitude and longitude information from the Google Maps server
 rgeo = requests.get(urlgeo + "address=" + address_string + "&key=" + api_key)
@@ -22,8 +22,8 @@ geo_json = json.load(fgeo)
 loc_lat  = geo_json["results"][0]["geometry"]["location"]["lat"]
 loc_lng  = geo_json["results"][0]["geometry"]["location"]["lng"]
 
-print (loc_lat)
-print (loc_lng)
+print ("This location's latitude is " + str(loc_lat))
+print ("This location's longitude is " + str(loc_lng))
 
 # requests relevant pizza restaurant information from Google Maps
 rplaces = requests.get(urlplaces + "location=" + str(loc_lat) + "," + str(loc_lng) + "&radius=5000&type=restaurant&keyword=pizza&key=" + api_key)
@@ -39,6 +39,7 @@ res_places = []
 for i in range(0, 10):
     res_places.append(places_json["results"][i]["name"])
 
+print ("The pizza restaurants in a 5km radius from your location are:")
 print (res_places)
 
 # closes the image file it wrote to
