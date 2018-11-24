656f16c616e50836461dc3395996787c5d2ff9b1
BenjaminBeggs <45314356+BenjaminBeggs@users.noreply.github.com>
2018-11-24 13:32:15 -0500

    parsing component completed

--- map.py
+++ map.py
@@ -1,7 +1,6 @@
 # returns relevant Google Maps data based on user-input location
 
-import requests
-import json
+import requests, json
 
 api_key = "AIzaSyClKuWY2YUpxRL_ypzI6CcN-U6KDjtHinY"
   
@@ -10,7 +9,7 @@ urlgeo 	  = "https://maps.googleapis.com/maps/api/geocode/json?"
 urlplaces = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"
 
 # user input of place string, this will be changed to the user input on the website
-address_string = "Queen's University"
+address_string = "stittsville ottawa"
 
 # requests relevant latitude and longitude information from the Google Maps server
 rgeo = requests.get(urlgeo + "address=" + address_string + "&key=" + api_key)
@@ -23,6 +22,9 @@ geo_json = json.load(fgeo)
 loc_lat  = geo_json["results"][0]["geometry"]["location"]["lat"]
 loc_lng  = geo_json["results"][0]["geometry"]["location"]["lng"]
 
+print (loc_lat)
+print (loc_lng)
+
 # requests relevant pizza restaurant information from Google Maps
 rplaces = requests.get(urlplaces + "location=" + str(loc_lat) + "," + str(loc_lng) + "&radius=5000&type=restaurant&keyword=pizza&key=" + api_key)
 
@@ -31,10 +33,13 @@ fplaces = open('pizzaplaces.txt', 'wb')
 fplaces.write(rplaces.content)
 fplaces = open('pizzaplaces.txt', 'r')
 
-# parses the restaurant names from Google Maps
+# parses the restaurant names from Google Maps, puts them to an array and prints them off
 places_json = json.load(fplaces)
-loc_lat  = geo_json["results"][0]["geometry"]["location"]["lat"]
-loc_lng  = geo_json["results"][0]["geometry"]["location"]["lng"]
+res_places = []
+for i in range(0, 10):
+    res_places.append(places_json["results"][i]["name"])
+
+print (res_places)
 
 # closes the image file it wrote to
 fgeo.close()
