e01cbbe0e5239d2cfd4c177c98524149543a181e
BenjaminBeggs <45314356+BenjaminBeggs@users.noreply.github.com>
2018-11-24 13:21:46 -0500

    Finished location parsing, working on restaurant parsing

--- map.py
+++ map.py
@@ -7,12 +7,12 @@ api_key = "AIzaSyClKuWY2YUpxRL_ypzI6CcN-U6KDjtHinY"
   
 # url variable store url 
 urlgeo 	  = "https://maps.googleapis.com/maps/api/geocode/json?"
-urlplaces = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=44.2253,-76.4951&radius=5000&type=restaurant&keyword=pizza&key=AIzaSyBF4V3_kp1x6Y6Ovalq4bmWfyokh_edgk4"
+urlplaces = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"
 
 # user input of place string, this will be changed to the user input on the website
 address_string = "Queen's University"
 
-# requests relevant information from the Google Maps server
+# requests relevant latitude and longitude information from the Google Maps server
 rgeo = requests.get(urlgeo + "address=" + address_string + "&key=" + api_key)
 fgeo = open('latlong.txt', 'wb')
 fgeo.write(rgeo.content)
@@ -20,18 +20,21 @@ fgeo = open('latlong.txt', 'r')
 
 # parses the latitude and longitude information from Google Maps
 geo_json = json.load(fgeo)
-loc_lat  = geo_json["lat"]["geometry"]["location"]["lat"]
-loc_lng  = geo_json["results"]["geometry"]["location"]["lng"]
+loc_lat  = geo_json["results"][0]["geometry"]["location"]["lat"]
+loc_lng  = geo_json["results"][0]["geometry"]["location"]["lng"]
 
-places = requests.get(urlplaces)
+# requests relevant pizza restaurant information from Google Maps
+rplaces = requests.get(urlplaces + "location=" + str(loc_lat) + "," + str(loc_lng) + "&radius=5000&type=restaurant&keyword=pizza&key=" + api_key)
 
-# opens the files it will write this information to, and subsequently writes to them
+# opens the restaurant file it will write this information to and subsequently writes to it
 fplaces = open('pizzaplaces.txt', 'wb')
-floc = open('locs.txt', 'wb')
-#fplaces.write(rplaces.content)
+fplaces.write(rplaces.content)
+fplaces = open('pizzaplaces.txt', 'r')
 
-#floc.write(loc_lng)
-#fplaces.write(loc_lat)
+# parses the restaurant names from Google Maps
+places_json = json.load(fplaces)
+loc_lat  = geo_json["results"][0]["geometry"]["location"]["lat"]
+loc_lng  = geo_json["results"][0]["geometry"]["location"]["lng"]
 
 # closes the image file it wrote to
 fgeo.close()
